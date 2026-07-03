8d2f792ad0975426300ab5baad6f9efc419a5f0d
Mansimishra444 <2k23.csai2312364@gmail.com>
2026-07-04 01:10:45 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ connectDB();
 const app= express();
 app.use(cors());
 app.use(express.json());
+const authRoutes=require("./routes/authRoutes");
+app.use("/api/auth",authRoutes);
 app.get('/', (req, res)=>{
     res.send("Career Ace backend is running");
 });
